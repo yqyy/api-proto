096ae92b49a87a493e5c3fc6cbf6de092e72e5dd
Gunyun <guanyun1024@icloud.com>
2021-08-26 17:11:01 +0800

    update

--- pkg/ts/svc.commission_pb_service.d.ts
+++ pkg/ts/svc.commission_pb_service.d.ts
@@ -1,3 +1,120 @@
 // package: commspb
 // file: svc.commission.proto
 
+import * as svc_commission_pb from "./svc.commission_pb";
+import {grpc} from "@improbable-eng/grpc-web";
+
+type CommissionServiceUserBindOrder = {
+  readonly methodName: string;
+  readonly service: typeof CommissionService;
+  readonly requestStream: false;
+  readonly responseStream: false;
+  readonly requestType: typeof svc_commission_pb.UserBindOrderReq;
+  readonly responseType: typeof svc_commission_pb.UserBindOrderRes;
+};
+
+type CommissionServiceUserBindPhone = {
+  readonly methodName: string;
+  readonly service: typeof CommissionService;
+  readonly requestStream: false;
+  readonly responseStream: false;
+  readonly requestType: typeof svc_commission_pb.UserBindPhoneReq;
+  readonly responseType: typeof svc_commission_pb.UserBindPhoneRes;
+};
+
+type CommissionServiceUserGetOrders = {
+  readonly methodName: string;
+  readonly service: typeof CommissionService;
+  readonly requestStream: false;
+  readonly responseStream: false;
+  readonly requestType: typeof svc_commission_pb.UserGetOrdersReq;
+  readonly responseType: typeof svc_commission_pb.UserGetOrdersRes;
+};
+
+type CommissionServiceUserWithdrawMoney = {
+  readonly methodName: string;
+  readonly service: typeof CommissionService;
+  readonly requestStream: false;
+  readonly responseStream: false;
+  readonly requestType: typeof svc_commission_pb.UserDrawMoneyReq;
+  readonly responseType: typeof svc_commission_pb.UserDrawMoneyReq;
+};
+
+export class CommissionService {
+  static readonly serviceName: string;
+  static readonly UserBindOrder: CommissionServiceUserBindOrder;
+  static readonly UserBindPhone: CommissionServiceUserBindPhone;
+  static readonly UserGetOrders: CommissionServiceUserGetOrders;
+  static readonly UserWithdrawMoney: CommissionServiceUserWithdrawMoney;
+}
+
+export type ServiceError = { message: string, code: number; metadata: grpc.Metadata }
+export type Status = { details: string, code: number; metadata: grpc.Metadata }
+
+interface UnaryResponse {
+  cancel(): void;
+}
+interface ResponseStream<T> {
+  cancel(): void;
+  on(type: 'data', handler: (message: T) => void): ResponseStream<T>;
+  on(type: 'end', handler: (status?: Status) => void): ResponseStream<T>;
+  on(type: 'status', handler: (status: Status) => void): ResponseStream<T>;
+}
+interface RequestStream<T> {
+  write(message: T): RequestStream<T>;
+  end(): void;
+  cancel(): void;
+  on(type: 'end', handler: (status?: Status) => void): RequestStream<T>;
+  on(type: 'status', handler: (status: Status) => void): RequestStream<T>;
+}
+interface BidirectionalStream<ReqT, ResT> {
+  write(message: ReqT): BidirectionalStream<ReqT, ResT>;
+  end(): void;
+  cancel(): void;
+  on(type: 'data', handler: (message: ResT) => void): BidirectionalStream<ReqT, ResT>;
+  on(type: 'end', handler: (status?: Status) => void): BidirectionalStream<ReqT, ResT>;
+  on(type: 'status', handler: (status: Status) => void): BidirectionalStream<ReqT, ResT>;
+}
+
+export class CommissionServiceClient {
+  readonly serviceHost: string;
+
+  constructor(serviceHost: string, options?: grpc.RpcOptions);
+  userBindOrder(
+    requestMessage: svc_commission_pb.UserBindOrderReq,
+    metadata: grpc.Metadata,
+    callback: (error: ServiceError|null, responseMessage: svc_commission_pb.UserBindOrderRes|null) => void
+  ): UnaryResponse;
+  userBindOrder(
+    requestMessage: svc_commission_pb.UserBindOrderReq,
+    callback: (error: ServiceError|null, responseMessage: svc_commission_pb.UserBindOrderRes|null) => void
+  ): UnaryResponse;
+  userBindPhone(
+    requestMessage: svc_commission_pb.UserBindPhoneReq,
+    metadata: grpc.Metadata,
+    callback: (error: ServiceError|null, responseMessage: svc_commission_pb.UserBindPhoneRes|null) => void
+  ): UnaryResponse;
+  userBindPhone(
+    requestMessage: svc_commission_pb.UserBindPhoneReq,
+    callback: (error: ServiceError|null, responseMessage: svc_commission_pb.UserBindPhoneRes|null) => void
+  ): UnaryResponse;
+  userGetOrders(
+    requestMessage: svc_commission_pb.UserGetOrdersReq,
+    metadata: grpc.Metadata,
+    callback: (error: ServiceError|null, responseMessage: svc_commission_pb.UserGetOrdersRes|null) => void
+  ): UnaryResponse;
+  userGetOrders(
+    requestMessage: svc_commission_pb.UserGetOrdersReq,
+    callback: (error: ServiceError|null, responseMessage: svc_commission_pb.UserGetOrdersRes|null) => void
+  ): UnaryResponse;
+  userWithdrawMoney(
+    requestMessage: svc_commission_pb.UserDrawMoneyReq,
+    metadata: grpc.Metadata,
+    callback: (error: ServiceError|null, responseMessage: svc_commission_pb.UserDrawMoneyReq|null) => void
+  ): UnaryResponse;
+  userWithdrawMoney(
+    requestMessage: svc_commission_pb.UserDrawMoneyReq,
+    callback: (error: ServiceError|null, responseMessage: svc_commission_pb.UserDrawMoneyReq|null) => void
+  ): UnaryResponse;
+}
+
--- pkg/ts/svc.commission_pb_service.js
+++ pkg/ts/svc.commission_pb_service.js
@@ -1,3 +1,181 @@
 // package: commspb
 // file: svc.commission.proto
 
+var svc_commission_pb = require("./svc.commission_pb");
+var grpc = require("@improbable-eng/grpc-web").grpc;
+
+var CommissionService = (function () {
+  function CommissionService() {}
+  CommissionService.serviceName = "commspb.CommissionService";
+  return CommissionService;
+}());
+
+CommissionService.UserBindOrder = {
+  methodName: "UserBindOrder",
+  service: CommissionService,
+  requestStream: false,
+  responseStream: false,
+  requestType: svc_commission_pb.UserBindOrderReq,
+  responseType: svc_commission_pb.UserBindOrderRes
+};
+
+CommissionService.UserBindPhone = {
+  methodName: "UserBindPhone",
+  service: CommissionService,
+  requestStream: false,
+  responseStream: false,
+  requestType: svc_commission_pb.UserBindPhoneReq,
+  responseType: svc_commission_pb.UserBindPhoneRes
+};
+
+CommissionService.UserGetOrders = {
+  methodName: "UserGetOrders",
+  service: CommissionService,
+  requestStream: false,
+  responseStream: false,
+  requestType: svc_commission_pb.UserGetOrdersReq,
+  responseType: svc_commission_pb.UserGetOrdersRes
+};
+
+CommissionService.UserWithdrawMoney = {
+  methodName: "UserWithdrawMoney",
+  service: CommissionService,
+  requestStream: false,
+  responseStream: false,
+  requestType: svc_commission_pb.UserDrawMoneyReq,
+  responseType: svc_commission_pb.UserDrawMoneyReq
+};
+
+exports.CommissionService = CommissionService;
+
+function CommissionServiceClient(serviceHost, options) {
+  this.serviceHost = serviceHost;
+  this.options = options || {};
+}
+
+CommissionServiceClient.prototype.userBindOrder = function userBindOrder(requestMessage, metadata, callback) {
+  if (arguments.length === 2) {
+    callback = arguments[1];
+  }
+  var client = grpc.unary(CommissionService.UserBindOrder, {
+    request: requestMessage,
+    host: this.serviceHost,
+    metadata: metadata,
+    transport: this.options.transport,
+    debug: this.options.debug,
+    onEnd: function (response) {
+      if (callback) {
+        if (response.status !== grpc.Code.OK) {
+          var err = new Error(response.statusMessage);
+          err.code = response.status;
+          err.metadata = response.trailers;
+          callback(err, null);
+        } else {
+          callback(null, response.message);
+        }
+      }
+    }
+  });
+  return {
+    cancel: function () {
+      callback = null;
+      client.close();
+    }
+  };
+};
+
+CommissionServiceClient.prototype.userBindPhone = function userBindPhone(requestMessage, metadata, callback) {
+  if (arguments.length === 2) {
+    callback = arguments[1];
+  }
+  var client = grpc.unary(CommissionService.UserBindPhone, {
+    request: requestMessage,
+    host: this.serviceHost,
+    metadata: metadata,
+    transport: this.options.transport,
+    debug: this.options.debug,
+    onEnd: function (response) {
+      if (callback) {
+        if (response.status !== grpc.Code.OK) {
+          var err = new Error(response.statusMessage);
+          err.code = response.status;
+          err.metadata = response.trailers;
+          callback(err, null);
+        } else {
+          callback(null, response.message);
+        }
+      }
+    }
+  });
+  return {
+    cancel: function () {
+      callback = null;
+      client.close();
+    }
+  };
+};
+
+CommissionServiceClient.prototype.userGetOrders = function userGetOrders(requestMessage, metadata, callback) {
+  if (arguments.length === 2) {
+    callback = arguments[1];
+  }
+  var client = grpc.unary(CommissionService.UserGetOrders, {
+    request: requestMessage,
+    host: this.serviceHost,
+    metadata: metadata,
+    transport: this.options.transport,
+    debug: this.options.debug,
+    onEnd: function (response) {
+      if (callback) {
+        if (response.status !== grpc.Code.OK) {
+          var err = new Error(response.statusMessage);
+          err.code = response.status;
+          err.metadata = response.trailers;
+          callback(err, null);
+        } else {
+          callback(null, response.message);
+        }
+      }
+    }
+  });
+  return {
+    cancel: function () {
+      callback = null;
+      client.close();
+    }
+  };
+};
+
+CommissionServiceClient.prototype.userWithdrawMoney = function userWithdrawMoney(requestMessage, metadata, callback) {
+  if (arguments.length === 2) {
+    callback = arguments[1];
+  }
+  var client = grpc.unary(CommissionService.UserWithdrawMoney, {
+    request: requestMessage,
+    host: this.serviceHost,
+    metadata: metadata,
+    transport: this.options.transport,
+    debug: this.options.debug,
+    onEnd: function (response) {
+      if (callback) {
+        if (response.status !== grpc.Code.OK) {
+          var err = new Error(response.statusMessage);
+          err.code = response.status;
+          err.metadata = response.trailers;
+          callback(err, null);
+        } else {
+          callback(null, response.message);
+        }
+      }
+    }
+  });
+  return {
+    cancel: function () {
+      callback = null;
+      client.close();
+    }
+  };
+};
+
+exports.CommissionServiceClient = CommissionServiceClient;
+
